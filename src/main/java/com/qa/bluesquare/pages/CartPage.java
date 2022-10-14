90ac1a3670a792cd71bad0c039bd5328ed5a7fa6
Rohit <rathorerohit95@gmail.com>
2022-10-14 16:15:33 +0530

    added cart

---
+++ src/main/java/com/qa/bluesquare/pages/CartPage.java
@@ -0,0 +1,14 @@
+package com.qa.bluesquare.pages;
+
+public class CartPage {
+	
+ public void CartPage()
+ {
+	 System.out.println("cart page construtor");
+ }
+ 
+ public void addToCart()
+ {
+	 System.out.println("add to card developed");
+ }
+}
