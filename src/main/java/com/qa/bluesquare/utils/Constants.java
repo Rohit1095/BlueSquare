08c1ff1e3852ad2eb45182071b9aee6b40f9a27f
Rohit <rathorerohit95@gmail.com>
2022-10-30 00:46:30 +0530

    updating the test case

--- src/main/java/com/qa/bluesquare/utils/Constants.java
+++ src/main/java/com/qa/bluesquare/utils/Constants.java
@@ -8,7 +8,7 @@ import java.util.List;
 	public class Constants {
 		
 		public static final int DEFAULT_TIME_OUT=5;
-		public static final String LOGIN_PAGE_TITLE="Account Login2";
+		public static final String LOGIN_PAGE_TITLE="Account Login";
 		public static final String ACCOUNT_PAGE_TITLE="My Account";
 		public static final int ACC_PAGE_SECTION_COUNT=4;
 		public static final List<String> ACC_PAGE_SECTION_LIST=Arrays.asList("My Account", 
